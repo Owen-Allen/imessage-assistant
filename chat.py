ad54a8857315c0c1710d0d3f5a2734829258480c
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2023-12-21 12:37:53 -0500

    changed query to inner join so that invalid phone numbers don't provide a response

--- chat.py
+++ chat.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import datetime, time
 import os
 import argparse
-
 import sqlite3
 
 DB_PATH = '/Users/owenallen/Library/Messages/chat.db'
@@ -22,33 +20,32 @@ def get_recent_message(phone: int):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
-    cursor.execute(f"""SELECT guid, text, date, is_from_me
-                       FROM message
-                       LEFT JOIN handle ON message.handle_id = handle.ROWID AND handle.id = {phone}
-                       WHERE is_from_me = 0
-                       ORDER BY date DESC
-                       LIMIT 1;
-                   """)
-    # Fetch all the results
+    cursor.execute(f'''select m.guid, m.text, m.date, m.is_from_me
+                      from message as m join handle as h on m.handle_id = h.ROWID
+                      and h.id = "{phone}"
+                      where m.is_from_me = 0
+                      order by date desc
+                      limit 1;
+                   ''')
+
     results = cursor.fetchall()
     if(len(results)):
-        message = Message(results[0][0], results[0][1], results[0][2], results[0][3])
+        message = Message(*results[0])
         return message
     else:
         print(f"Could not find any messages from {phone}. Do you have the correct phone number?")
-        return
+        return False
 
 
 
-def generate_response():
+def generate_response(message: Message):
     # some api call
-
     return "Sure! That sounds awesome"
 
 
 def send_text(message: str, phone: str):
-
-    os.system(f"imessage --text {message} --contacts {phone}")
+    # os.system(f"imessage --text {message} --contacts {phone}")
+    return
 
 def main():
     # parse phone # from user input
@@ -62,6 +59,8 @@ def main():
 
     print(f"Generating a response for recent texts with, {phone}!")
     message = get_recent_message(phone)
+    print(message)
+    if(not message): return
     response = generate_response(message)
 
     # check if user it using safe mode?
@@ -70,5 +69,39 @@ def main():
 
 
 
+def tester():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    print("VALID QUERY")
+    cursor.execute('''
+                 select m.text, h.id
+                 from message as m join handle as h on m.handle_id = h.ROWID
+                 and h.id = "+19054073037"
+                 where m.is_from_me = 0
+                 order by date desc
+                 limit 1;
+                 ''')
+
+    results = cursor.fetchall()
+    print(results)
+    print()
+    print("INVALID QUERY")
+
+    cursor.execute('''
+                select m.text, h.id
+                from message as m join handle as h on m.handle_id = h.ROWID
+                and h.id = "+134582345982348423"
+                where m.is_from_me = 0
+                order by date desc
+                limit 1;
+                 ''')
+
+    results = cursor.fetchall()
+    print(results)
+    print()
+
+
 if __name__ == "__main__":
     main()
+    # tester()
