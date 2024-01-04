b4516a419b6b1b994dea49046a8277075c0aaf8e
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2024-01-04 15:07:44 -0500

    added get_recent_messages and get_messages_since functions

--- src/chat.py
+++ src/chat.py
@@ -1,29 +1,55 @@
 import sqlite3
-
+from .message import Message, MessageList
 
 DB_PATH = '/Users/owenallen/Library/Messages/chat.db'
 
-
 class ChatAPI:
     '''
-    An API for specifiy queries the iMessage chat.db
+    An API for querying the iMessage chat.db
     '''
 
-    def get_recent_incoming_message(phone_number: int):
-        conn = sqlite3.connect(DB_PATH)
+    def __init__(self, db_path):
+        self.db_path = db_path
+
+    def get_recent_messages(self, phone_number, quantity):
+        conn = sqlite3.connect(self.db_path)
         cursor = conn.cursor()
 
         cursor.execute(f'''select m.guid, m.text, m.date, m.is_from_me
                         from message as m join handle as h on m.handle_id = h.ROWID
                         and h.id = "{phone_number}"
-                        where m.is_from_me = 0
                         order by date desc
-                        limit 1;
+                        limit {quantity};
                     ''')
-
+        
         results = cursor.fetchall()
-        if(len(results)):
-            message = Message(*results[0])
-            return message
-        else:
-            raise Exception(f"No message found from {phone_number}")
+        conn.close()
+        
+        messages = MessageList()
+        for result in results:
+            message = Message(*result)
+            messages.append(message)
+
+        return messages
+
+    def get_messages_since(self, phone_number, date: int):
+
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+
+        cursor.execute(f'''SELECT m.guid, m.text, m.date, m.is_from_me
+                FROM message AS m JOIN handle AS h ON m.handle_id = h.ROWID
+                WHERE h.id = "{phone_number}" AND m.date > {date}
+                ORDER BY m.date ASC;
+                    ''')
+        
+        results = cursor.fetchall()
+        conn.close()
+        
+        messages = MessageList()
+        for result in results:
+            message = Message(*result)
+            messages.append(message)
+
+        return messages
+
