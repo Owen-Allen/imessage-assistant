10dab2f1766b14d4792ad369b913e1f02beff891
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2024-01-05 16:19:28 -0500

    changed get structure from Conversation to allow multiple user messages at a time. Added try catch.

--- src/main.py
+++ src/main.py
@@ -2,107 +2,99 @@
 import os
 import time
 import json
+import logging
 from dotenv import load_dotenv
 from openai import OpenAI
-import shlex
-
-
 from .message import Message, MessageList
 from .conversation import Conversation
 from .cli import ArgParseWrapper
 from .chat import ChatAPI
 
+# Load environment variables
+load_dotenv()
 
-DB_PATH = "/Users/owenallen/Library/Messages/chat.db"
-
-
+DB_PATH = os.getenv('DB_PATH', '/Users/owenallen/Library/Messages/chat.db')
 openai_api_key = os.getenv('OPENAI_API_KEY')
 
+# Initialize OpenAI client
 client = OpenAI(api_key=openai_api_key)
 
-
 def send_text(message: str, phone_number: str):
-    print("sending text")
-
+    logging.info("Sending text")
     safe_message = message.replace("'", "'\\''")
-    print(safe_message)
     os.system(f"imessage --text '{safe_message}' --contacts {phone_number}")
-    return
-
-def show_json(obj):
-    json_object = json.loads(obj.model_dump_json())
-    json_formatted_str = json.dumps(json_object, indent=2)
-    print(json_formatted_str)
 
 def wait_on_run(run, thread):
-    while run.status == "queued" or run.status == "in_progress":
-        run = client.beta.threads.runs.retrieve(
-            thread_id=thread.id,
-            run_id=run.id,
-        )
+    while run.status in ["queued", "in_progress"]:
+        run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
         time.sleep(0.5)
     return run
 
+def load_instructions(file_path="instructions"):
+    try:
+        with open(file_path, "r") as f:
+            return f.read()
+    except IOError:
+        logging.error(f"Failed to read instructions from {file_path}")
+        return ""
 
 def main():
+    logging.basicConfig(level=logging.INFO)
     parser = ArgParseWrapper()
     args = parser.parse_args()
-
     phone_number = args.phone
+
     chat_db = ChatAPI(DB_PATH)
-
     conversation = Conversation(args.phone, chat_db)
-
-    instructions = ""
-    with open("instructions", "r") as f:
-        instructions = f.read()
+    instructions = load_instructions()
 
     assistant = client.beta.assistants.create(
         name="iMessage Assistant",
         instructions=instructions,
         model="gpt-3.5-turbo"
     )
-
     thread = client.beta.threads.create()
 
-
-    # thread created, start feeding messages to assistant
     last_message_id = -1
-    while True:
-        # prepare message for assistant
-        message_from_user = conversation.get_latest_from_user()
+    try:
+        while True:
+            conversation.refresh_messages()
+            messages_from_user = conversation.get_messages_after(last_message_id)
+            if len(messages_from_user) == 0:
+                time.sleep(5)
+                continue
 
-        if(message_from_user.guid == last_message_id):
+            last_message_id = messages_from_user[-1].guid
+
+            combined_message_text = ""
+
+            for message in messages_from_user:
+                combined_message_text += message.text + "  "
+
+            client.beta.threads.messages.create(
+                        thread_id=thread.id,
+                        role="user",
+                        content=combined_message_text,
+            )
+
+            # send assistant a message
+            run = client.beta.threads.runs.create(
+                thread_id=thread.id,
+                assistant_id=assistant.id,
+            )
+            wait_on_run(run, thread)
+
+            messages = client.beta.threads.messages.list(thread_id=thread.id)
+            message_to_user = messages.data[0].content[0].text.value
+            if(message_to_user == "STOP"):
+                break
+        
+            send_text(message_to_user, phone_number)
             time.sleep(5)
-            continue
-
-        last_message_id = message_from_user.guid
-
-        client.beta.threads.messages.create(
-                    thread_id=thread.id,
-                    role="user",
-                    content=message_from_user.text,
-        )
-
-        # send assistant a message
-        run = client.beta.threads.runs.create(
-            thread_id=thread.id,
-            assistant_id=assistant.id,
-        )
-        wait_on_run(run, thread)
-
-        messages = client.beta.threads.messages.list(thread_id=thread.id)
-        message_to_user = messages.data[0].content[0].text.value
-        send_text(message_to_user, phone_number)
-        time.sleep(5)
-        conversation.refresh_messages()
-        # show_json(messages)
-
-    # start a thread
-    # create the initial message, which is tied to the thread
-    # create a run, by tying the thread to the assistant
-    # start loop?
-
+    except KeyboardInterrupt:
+        logging.info("Program terminated by user")
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
